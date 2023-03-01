65a7403e564bfb3f7205bef0173d1865c1036099
hyperplayer7 <hyperplayer7@yahoo.com>
2023-03-01 10:06:41 +0800

    display contents based on the api provided

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,190 @@
+// import Head from 'next/head';
+// import styles from '../styles/Home.module.css';
+
+// export default function Home() {
+//   return (
+//     <div className={styles.container}>
+//       <Head>
+//         <title>Create Next App</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+
+//       <main>
+//         <h1 className={styles.title}>
+//           Welcome to <a href="https://nextjs.org">Next.js!</a>
+//         </h1>
+
+//         <p className={styles.description}>
+//           Get started by editing <code>pages/index.js</code>
+//         </p>
+
+//         <div className={styles.grid}>
+//           <a href="https://nextjs.org/docs" className={styles.card}>
+//             <h3>Documentation &rarr;</h3>
+//             <p>Find in-depth information about Next.js features and API.</p>
+//           </a>
+
+//           <a href="https://nextjs.org/learn" className={styles.card}>
+//             <h3>Learn &rarr;</h3>
+//             <p>Learn about Next.js in an interactive course with quizzes!</p>
+//           </a>
+
+//           <a
+//             href="https://github.com/vercel/next.js/tree/master/examples"
+//             className={styles.card}
+//           >
+//             <h3>Examples &rarr;</h3>
+//             <p>Discover and deploy boilerplate example Next.js projects.</p>
+//           </a>
+
+//           <a
+//             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+//             className={styles.card}
+//           >
+//             <h3>Deploy &rarr;</h3>
+//             <p>
+//               Instantly deploy your Next.js site to a public URL with Vercel.
+//             </p>
+//           </a>
+//         </div>
+//       </main>
+
+//       <footer>
+//         <a
+//           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Powered by{' '}
+//           <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+//         </a>
+//       </footer>
+
+//       <style jsx>{`
+//         main {
+//           padding: 5rem 0;
+//           flex: 1;
+//           display: flex;
+//           flex-direction: column;
+//           justify-content: center;
+//           align-items: center;
+//         }
+//         footer {
+//           width: 100%;
+//           height: 100px;
+//           border-top: 1px solid #eaeaea;
+//           display: flex;
+//           justify-content: center;
+//           align-items: center;
+//         }
+//         footer img {
+//           margin-left: 0.5rem;
+//         }
+//         footer a {
+//           display: flex;
+//           justify-content: center;
+//           align-items: center;
+//           text-decoration: none;
+//           color: inherit;
+//         }
+//         code {
+//           background: #fafafa;
+//           border-radius: 5px;
+//           padding: 0.75rem;
+//           font-size: 1.1rem;
+//           font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+//             DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+//         }
+//       `}</style>
+
+//       <style jsx global>{`
+//         html,
+//         body {
+//           padding: 0;
+//           margin: 0;
+//           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+//             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+//             sans-serif;
+//         }
+//         * {
+//           box-sizing: border-box;
+//         }
+//       `}</style>
+//     </div>
+//   )
+// }
+
+
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function Resume() {
+  const [resumeData, setResumeData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/hyperplayer7/jsonresume/main/resume.json')
+      .then(res => res.json())
+      .then(data => setResumeData(data))
+      .catch(error => console.error(error));
+  }, []);
+
+  if (!resumeData) {
+    return <div>Loading...</div>;
+  }
+
+  const {
+    basics: { name, summary, email, profiles },
+    projects,
+    skills,
+    work,
+  } = resumeData;
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{name} - Resume</title>
       </Head>
-
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <h1>{name}</h1>
+      <p>{summary}</p>
+      <p>Email: {email}</p>
+      <h2>Profiles</h2>
+      <ul>
+        {profiles.map(profile => (
+          <li key={profile.network}>
+            {profile.network}: {profile.url}
+          </li>
+        ))}
+      </ul>
+      <h2>Projects</h2>
+      <ul>
+        {projects.map(project => (
+          <li key={project.name}>
+            <a href={project.url}>{project.name}</a>
+          </li>
+        ))}
+      </ul>
+      <h2>Skills</h2>
+      <ul>
+        {skills.map(skill => (
+          <li key={skill.name}>
+            {skill.name}: {skill.keywords.join(', ')}
+          </li>
+        ))}
+      </ul>
+      <h2>Work Experience</h2>
+      {work.map(job => (
+        <div key={job.position}>
+          <h3>
+            {job.position} at {job.name}
+          </h3>
+          <p>
+            {job.startDate} - {job.endDate}
+          </p>
+          {job.highlights.map(highlight => (
+            <p key={highlight}>{highlight}</p>
+          ))}
         </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      ))}
     </div>
-  )
+  );
 }
